df61cbc01fe49af86e2d9bea0bf3d9cafcbb897f
jimmy_cmc_2015@icloud.com <jimmy_cmc_2015@icloud.com>
2016-10-08 16:30:59 +0800

    屏幕适配

--- app/src/main/java/com/jeek/calendar/widget/calendar/schedule/ScheduleLayout.java
+++ app/src/main/java/com/jeek/calendar/widget/calendar/schedule/ScheduleLayout.java
@@ -1,7 +1,6 @@
 package com.jeek.calendar.widget.calendar.schedule;
 
 import android.content.Context;
-import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
